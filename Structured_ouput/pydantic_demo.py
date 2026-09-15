a47dea9eab5435120e3a87ef3dfd76c5ecf12639
vimalsingh135 <be23b042@smail.iitm.ac.in>
2026-09-15 17:31:01 +0530

    learning and implementing Json_Schema

--- Structured_ouput/pydantic_demo.py
+++ Structured_ouput/pydantic_demo.py
@@ -3,13 +3,13 @@ from typing import Optional
 
 class Student(BaseModel):
 
-    name: str = 'nitish'
+    name: str = 'vimal singh'
     age: Optional[int] = None
     email: EmailStr
     cgpa: float = Field(gt=0, lt=10, default=5, description='A decimal value representing the cgpa of the student')
 
 
-new_student = {'age':'32', 'email':'abc@gmail.com'}
+new_student = {'age':22, 'email':'abc@gmail.com'}
 
 student = Student(**new_student)
 
